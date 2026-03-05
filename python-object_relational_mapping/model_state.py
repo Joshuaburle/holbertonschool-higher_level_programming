7810e7246a8b132eb6ac9b17b9a1cee81c30387c
Joshuaburle <11491@holbertonschool.com>
2026-03-05 23:36:19 +0100

    6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,8 +2,7 @@
 """Defines the State class and Base for SQLAlchemy ORM."""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -14,5 +13,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
