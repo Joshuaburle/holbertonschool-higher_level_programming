f41fbe553ec0bdcfa3e17916e35f23ec3eacf521
Joshuaburle <11491@holbertonschool.com>
2026-03-20 15:41:07 +0100

    Add else loop to file 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 
 if (process.argv[2] === undefined) {
   console.log('No argument');
+} else if (process.argv[3] === undefined) {
+  console.log('Argument found');
 } else {
-  console.log(process.argv[2]);
+  console.log('Arguments found');
 }
